b30105415974e7b5486b9f43dc70d353088e0dc5
CloudyCloud <141394536+AwfullyCloudy@users.noreply.github.com>
2025-12-10 08:16:51 +0800

    Created computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+console.log(getComputerChoice())
+
+
+function getComputerChoice() {
+    let choice, choiceMath;
+    choiceMath = Math.random();
+    if (choiceMath <= 0.33) {
+        choice = "Rock";
+    }
+    else if (choiceMath > 0.33 && choiceMath<= 0.66) {
+        choice = "Paper";
+    }
+    else {
+        choice = "Scissors";
+    }
+    return choice;
+}
