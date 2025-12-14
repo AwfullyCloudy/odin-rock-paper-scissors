f70ada0db1927ab723f0a3d284f416c3292035ac
CloudyCloud <141394536+AwfullyCloudy@users.noreply.github.com>
2025-12-14 21:46:24 +0800

    Added playRound() and playGame()

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log(getComputerChoice())
-
+playGame();
 
 function getComputerChoice() {
     let choice, choiceMath;
@@ -14,4 +13,71 @@ function getComputerChoice() {
         choice = "Scissors";
     }
     return choice;
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock, paper, or scissors?");
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let roundWinner = '';
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("PC Chose Paper! You Chose Rock! You Lose!");
+        roundWinner = 'C';
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("PC Chose Scissors! You Chose Rock! You Win!");
+        roundWinner = 'H';
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("PC Chose Scissors! You Chose Paper! You Lose!");
+        roundWinner = 'C';
+
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("PC Chose Rock! You Chose Paper! You Win!");
+        roundWinner = 'H';
+
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("PC Chose Rock! You Chose Scissors! You Lose!");
+        roundWinner = 'C';
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("PC Chose Paper! You Chose Scissors! You Win!");
+        roundWinner = 'H';
+    }
+    else {
+        console.log("Tie!");
+        roundWinner = 'T';
+    }
+    return roundWinner;
+}
+
+function playGame() {
+    let humanScore = 0, computerScore = 0;
+    let roundWinner;
+    for (let i = 0; i < 5; i++) {
+        roundWinner = playRound(getHumanChoice(), getComputerChoice());
+
+        if (roundWinner == 'H') {
+            humanScore += 1;
+        }
+        else if (roundWinner = 'C') {
+            computerScore += 1;
+        }
+    }
+    if (humanScore > computerScore) {
+        console.log("Human wins!");
+    }
+    else if (computerScore > humanScore) {
+        console.log("Machine wins!");
+    }
+    else {
+        console.log("Its a tie!");
+    }
 }
